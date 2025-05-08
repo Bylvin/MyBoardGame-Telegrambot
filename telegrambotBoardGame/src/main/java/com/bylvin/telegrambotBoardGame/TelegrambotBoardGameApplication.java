afe9f7ed68f755429c18d84929ad572ebc43816f
Bylvin <carel.raditya@gmail.com>
2025-05-08 13:13:00 +0700

    Membuat project spring-boot

---
+++ telegrambotBoardGame/src/main/java/com/bylvin/telegrambotBoardGame/TelegrambotBoardGameApplication.java
@@ -0,0 +1,13 @@
+package com.bylvin.telegrambotBoardGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrambotBoardGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegrambotBoardGameApplication.class, args);
+	}
+
+}
